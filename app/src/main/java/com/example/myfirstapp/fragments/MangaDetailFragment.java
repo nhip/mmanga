46b334c4ae3afcc373535d699e13894d88fce2f0
nhip <nhi.phuong@zu.com>
2017-10-18 13:42:06 -0600

    Added some more things.

--- app/src/main/java/com/example/myfirstapp/fragments/MangaDetailFragment.java
+++ app/src/main/java/com/example/myfirstapp/fragments/MangaDetailFragment.java
@@ -10,35 +10,11 @@ import android.view.ViewGroup;
 
 import com.example.myfirstapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MangaDetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MangaDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MangaDetailFragment extends Fragment {
 
     public MangaDetailFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MangaDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MangaDetailFragment newInstance(String param1, String param2) {
-        MangaDetailFragment fragment = new MangaDetailFragment();
-        Bundle args = new Bundle();
-
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
